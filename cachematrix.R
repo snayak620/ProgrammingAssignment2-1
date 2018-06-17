f4e6e03e077351a8befa3f7b83f017059daad0ec
snayak620 <sareet.nayak@gmail.com>
2018-06-17 17:58:03 -0400

    My Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix is a funciton that will create a matrix that can cache its inverse
+## cachSolve is a function that will compute the inverse through the previous function, otherwise it will retrieve the inverse 
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x=matrix())  ## creates and computers an object matrix which can find/cache its inverse
+	inversematrix <- NULL
+	set <- function(y){
+		x <<- y  
+		inversematrix <<- NULL 
+		}
+	get <- function()x
+	setinverse <- function(inverse) inversematrix <<- inverse
+	setinverse <- function() inversematrix
+	list (set = set, get = get,
+		setinverse = setinverse, 
+		getinverse= getinverse) 
+		}
+cacheSolve <- function (x,...){ ##computes the inverse, otherwise retrieves the inverse from the cache 
+	inversematrix <- x$getinverse()
+	if($is.nULL(inversematrix){
+		message ("finding cached data")
+		return (inversematrix) 
+	}
+	datam <- x$get() 
+	inversematrix <- solve (datam)
+	x$setinverse(inversematrix)
+	inversematrix
 }
+## testing
+x = rbind(c(1,2), c(3,4))
+m = makeCacheMatrix(x)
+m$get() 
+ 
+		
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+		
